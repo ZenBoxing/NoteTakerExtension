bcf1bd9945cc18b4d768daab9bbffb2f4cfd466c
Jarrett Lirette <jarrettlirette@gmail.com>
2022-10-31 13:51:29 -0400

    Added onMessage Listener to background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-
+import {config} from './config.js';
 
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 //     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
@@ -12,6 +12,52 @@
 //             .catch(err => console.log(err));
 //     }
 // });
+
+const API_KEY = config.API_KEY;
+let documentID = "1vBLt4axCgXZ_aYqTfvYXTj-MroWI79fPWrR3uG1GARs";
+let user_signed_in = false;
+
+//possibly don't need this listener
+chrome.identity.onSignInChanged.addListener((account_id, signedIn) => {
+    if(signedIn){
+        user_signed_in = true;
+    } else {
+        user_signed_in = false;
+    }
+});
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    
+    if(request.message === 'append_document'){
+        chrome.identity.getAuthToken({ interactive: true }, (token) => {
+            let fetch_url =  `https://docs.googleapis.com/v1/documents/${documentID}:batchUpdate?key=${API_KEY}`;
+
+            let fetch_options = {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    requests: [
+                    {
+                        insertText: {
+                        text: "test this out",
+                        location: {
+                            index: 1,
+                        },
+                        },
+                    },
+                    ],
+                }),
+                };
+
+            fetch(fetch_url, fetch_options).then(res => res.json()).then(res => console.log(res));
+
+        });
+    }    
+});
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
