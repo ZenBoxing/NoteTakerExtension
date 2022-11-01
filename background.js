277c4c7274f18b4877979e412a8a53185a838203
Jarrett Lirette <jarrettlirette@gmail.com>
2022-11-01 09:43:47 -0400

    background.js now receives documentId from extension popup

--- background.js
+++ background.js
@@ -1,21 +1,8 @@
 import {config} from './config.js';
 
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-//         chrome.scripting.executeScript({
-//             target: { tabId: tabId },
-//             files: ["./main.js"]
-//         })
-//             .then(() => {
-//                 console.log("INJECTED THE FOREGROUND SCRIPT.");
-//             })
-//             .catch(err => console.log(err));
-//     }
-// });
-
 const API_KEY = config.API_KEY;
-let documentID = "1vBLt4axCgXZ_aYqTfvYXTj-MroWI79fPWrR3uG1GARs";
-let user_signed_in = false;
+let documentId = "1vBLt4axCgXZ_aYqTfvYXTj-MroWI79fPWrR3uG1GARs";
+let user_signed_in = false;// probably don't need this property
 
 //possibly don't need this listener
 chrome.identity.onSignInChanged.addListener((account_id, signedIn) => {
@@ -29,33 +16,8 @@ chrome.identity.onSignInChanged.addListener((account_id, signedIn) => {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     
-    if(request.message === 'append_document'){
-        chrome.identity.getAuthToken({ interactive: true }, (token) => {
-            let fetch_url =  `https://docs.googleapis.com/v1/documents/${documentID}:batchUpdate?key=${API_KEY}`;
-
-            let fetch_options = {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    requests: [
-                    {
-                        insertText: {
-                        text: "test this out",
-                        endOfSegmentLocation: {
-                            
-                        },
-                        },
-                    },
-                    ],
-                }),
-                };
-
-            fetch(fetch_url, fetch_options).then(res => res.json()).then(res => console.log(res));
-
-        });
+    if(request.message === 'load_document'){
+        documentId = request.documentId;
     }    
 });
 
@@ -76,7 +38,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     console.log(info.selectionText);
 
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
-        let fetch_url =  `https://docs.googleapis.com/v1/documents/${documentID}:batchUpdate?key=${API_KEY}`;
+        let fetch_url =  `https://docs.googleapis.com/v1/documents/${documentId}:batchUpdate?key=${API_KEY}`;
 
         let fetch_options = {
             method: "POST",
@@ -89,8 +51,8 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
                 {
                     insertText: {
                     text: "/n" + info.selectionText + "/n",
-                    location: {
-                        index: 1,
+                    endOfSegmentLocation: {
+                        
                     },
                     },
                 },
--- popup.js
+++ popup.js
@@ -1,41 +1,14 @@
-//import {config} from './config.js';
+//use queryselector instead of getElementById?
+
 
 document.getElementById("MainButton").addEventListener("click", onButtonClick);
 
 function onButtonClick(){
-    
-    
-      chrome.runtime.sendMessage({ message: 'append_document'});
-
-    // console.log("clicked");
-    // alert("clicked");
-    // const documentID = document.getElementById("Test").textContent;
-    
-    // let fetch_url = `https://docs.googleapis.com/v1/documents/${documentID}:batchUpdate?key=${config.API_KEY}`;
-
-    // let fetch_options = {
-    //   method: "POST",
-    //   headers: {
-    //     Authorization: `Bearer ${config.Token}`,
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     requests: [
-    //       {
-    //         insertText: {
-    //           text: "test this out",
-    //           location: {
-    //             index: 1,
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   }),
-    // };
-
-
-    // fetch(fetch_url, fetch_options).then(response => response.json()).then(response => { document.getElementById("log").textContent = response.statusText;});
-    // document.getElementById("end").textContent = "end of function";
+     
+     chrome.runtime.sendMessage({ 
+      message: 'load_document',
+      documentId: document.getElementById("documentId").value
+    });
   };
 
  
